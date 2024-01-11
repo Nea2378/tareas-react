cb56bc2d146956d1e295465bcd340956002e7882
Nancy Estrada <140356008+Nea2378@users.noreply.github.com>
2024-01-10 21:25:44 -0800

    fin del ejercicio

--- src/pages/Detalle.jsx
+++ src/pages/Detalle.jsx
@@ -1,5 +1,102 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { useParams } from "react-router-dom";
+
 function Detalle() {
-    return ( <h1>Detalle</h1> );
+    const [formData, setFormData] = useState({
+        tarea: "",
+        usuario: "nancye"
+    });
+
+    const headers = {
+        'Authorization': '9faa4f2eed9b6c5f9a748d54ed32cc90'
+    }
+    
+    const {id} = useParams();
+
+    useEffect(()=>{
+        //cargar el registo que tenga ese id...
+        const cargarRegistro = async ()=>{
+            try {
+                const response = await axios.get(`https://dev4humans.com.mx/api/clases/tareas?usuario=nancye&id=${id}`,{headers});
+                setFormData(response.data.data);
+                console.log(response);
+            } catch (error) {
+                console.log("Error en carga de informacion", error); 
+                }
+        }
+        if (id){
+            cargarRegistro();
+        }
+    }, [id])
+    
+   
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setFormData({
+            ...formData,
+            [name]: value
+        });
+    };
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        console.log(formData);
+        try {
+            if (id) {
+                const response = await axios
+                .put(
+                    `https://dev4humans.com.mx/api/clases/tareas?
+                    usuario=nancye&id=${id}`,
+                    formData,
+                    { headers }
+                );
+                console.log(response);
+            } else {
+
+            const response = await axios 
+            .post(
+                `https://dev4humans.com.mx/api/clases/tareas`,
+                formData,
+                { headers }
+            );
+        console.log(response);
+            }
+    } catch (error) {
+            console.log("Error en carga de informacion", error);
+        }
+    }
+    return (
+
+
+        <>
+            <div className="container">
+                <form onSubmit={handleSubmit}>
+                    <div className="mb-3">
+                        <label htmlFor="exampleInputEmail1" className="form-label">Tarea</label>
+                        <input
+                            type="text"
+                            className="form-control"
+                            name="tarea"
+                            value={formData.tarea}
+                            onChange={handleChange}
+                        />
+                    </div>
+                    <div className="mb-3">
+                        <label htmlFor="exampleInputPassword1" className="form-label">Usuario</label>
+                        <input
+                            type="text"
+                            className="form-control"
+                            name="usuario"
+                            value={formData.usuario}
+                            onChange={handleChange}
+                        />
+                    </div>
+                    <button type="submit" className="btn btn-primary">Guardar</button>
+                </form>
+            </div>
+        </>
+    );
 }
 
 export default Detalle;
--- src/pages/Listado.jsx
+++ src/pages/Listado.jsx
@@ -1,5 +1,91 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+import Loader from '..//components/Loader';
+import { Link } from "react-router-dom";
+
 function Listado() {
-    return (<h1>Listado</h1>  );
+
+    const [tareas, setTareas] = useState([]);
+    const [loader, setLoader] = useState(true);
+
+    const headers = {
+        'Authorization': '9faa4f2eed9b6c5f9a748d54ed32cc90'
+    }
+
+    const getTareas = async () => {
+        setLoader(true);
+        try {
+            const response = await axios.get(`https://dev4humans.com.mx/api/clases/tareas?usuario=nancye`, { headers });
+            setTareas(response.data.data);
+            console.log(response);
+        } catch (error) {
+            console.log("Error en carga de informacion", error);
+        }
+        setLoader(false);
+    };
+
+    useEffect(() => {
+        console.log("Entro a useEffect aqui deberia cargar mis tareas");
+        getTareas();
+    }, []);
+
+    const eliminarTarea = async (id) => {
+        console.log("El id es el quieres eliminar es" + id);
+        try {
+        await axios.delete(`https://dev4humans.com.mx/api/clases/tareas?usuario=nancye&id=${id}`,{headers});
+            getTareas();
+    } catch (error) {
+            console.log("Error en carga de informacion", error); 
+            }
+    }
+
+    if (loader) {
+        return (<Loader />)
+    }
+
+    return (
+        <div className="container">
+            <table className="table table-bordered table-striped">
+                <thead className="table-dark">
+                    <tr>
+                        <th>#</th>
+                        <th>Fecha</th>
+                        <th>Tarea</th>
+                        <th className="text-center">Acciones</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        tareas.map((tarea, index) => (
+                            <tr key={index}>
+                                <td>{index + 1}</td>
+                                <td>{tarea.fecha_registros}</td>
+                                <td>{tarea.tarea}</td>
+                                <td className="text-center">
+                                    <Link className="btn btn-info me-2" to={`/editar/${tarea.id}`}>Editar</Link>
+                                    <button type="button" className="btn btn-danger"
+                                    onClick={()=>eliminarTarea(tarea.id)}>
+                                        Eliminar
+                                    </button>
+                                </td>
+                            </tr>
+                        ))
+
+                    }
+                    {
+                        tareas.length === 0 && (
+                            <tr>
+                                <td colSpan={4} className="text-center">
+                                    No hay tareas
+                                </td>
+                            </tr>
+                        )
+                    }
+                </tbody>
+            </table>
+        </div >
+    );
 }
 
 export default Listado;
